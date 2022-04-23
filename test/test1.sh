a9e2c6d6f9c3eb676649409bfba7ff21da0257a2
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2022-04-22 21:40:52 -0700

    Update test1.sh

--- test/test1.sh
+++ test/test1.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/bash
 
 ## dot test
+echo "dot test";
+echo ". --version";
 node . --version;
 
-## ver test
+echo "";
+echo "version tests";
+## version test
+echo "pbj --version";
 node ./pbj-cli.js --version;
+echo "pbjson --version";
 node ./pbjson-cli.js --version;
 
 ## clone pinyinbase or a similar repo
+echo "pinyinbase working directory test";
 if test -d pinyinbase; then echo "pb-dir-FOUND"; else echo "pb-dir-GET"; git clone https://github.com/pffy/pinyinbase; fi
 
 ## simple outfile
+echo "simple outfile tests";
 node ./pbj-cli.js -i ./pinyinbase/vocab/*.txt
 node ./pbjson-cli.js -i ./pinyinbase/vocab/*.txt
 
 ## dated outfile
+echo "dated outfile tests";
 node ./pbj-cli.js -d -i ./pinyinbase/vocab/*.txt
 node ./pbjson-cli.js -d -i ./pinyinbase/vocab/*.txt
-
-
-# node pbjson;
